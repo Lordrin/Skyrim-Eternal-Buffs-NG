429b2aeaf640f90ac66893efb554e6312338382e
Lordrin_CK <chriskeen12@gmail.com>
2026-08-06 21:44:35 +0200

    removed unused code

--- src/UI.cpp
+++ src/UI.cpp
@@ -15,168 +15,6 @@ namespace {
             ImGuiMCP::EndTooltip();
         }
     }
-
-    bool Slider(const char* label, float& value, float min, float max, const char* format) {
-        ImGuiMCP::TextUnformatted(label);
-        ImGuiMCP::SameLine(130.0f);
-        ImGuiMCP::SetNextItemWidth(260.0f);
-        return ImGuiMCP::SliderFloat((std::string("##") + label).c_str(), &value, min, max, format);
-    }
-
-    bool captureListening = false;
-    bool captureArmed = false;
-
-    int PollCapturedInput() {
-        struct KeyMap {
-            int key;
-            int dik;
-        };
-        static const KeyMap keys[] = {{ImGuiMCP::ImGuiKey_Escape, 1},
-                                      {ImGuiMCP::ImGuiKey_1, 2},
-                                      {ImGuiMCP::ImGuiKey_2, 3},
-                                      {ImGuiMCP::ImGuiKey_3, 4},
-                                      {ImGuiMCP::ImGuiKey_4, 5},
-                                      {ImGuiMCP::ImGuiKey_5, 6},
-                                      {ImGuiMCP::ImGuiKey_6, 7},
-                                      {ImGuiMCP::ImGuiKey_7, 8},
-                                      {ImGuiMCP::ImGuiKey_8, 9},
-                                      {ImGuiMCP::ImGuiKey_9, 10},
-                                      {ImGuiMCP::ImGuiKey_0, 11},
-                                      {ImGuiMCP::ImGuiKey_Minus, 12},
-                                      {ImGuiMCP::ImGuiKey_Equal, 13},
-                                      {ImGuiMCP::ImGuiKey_Backspace, 14},
-                                      {ImGuiMCP::ImGuiKey_Tab, 15},
-                                      {ImGuiMCP::ImGuiKey_Q, 16},
-                                      {ImGuiMCP::ImGuiKey_W, 17},
-                                      {ImGuiMCP::ImGuiKey_E, 18},
-                                      {ImGuiMCP::ImGuiKey_R, 19},
-                                      {ImGuiMCP::ImGuiKey_T, 20},
-                                      {ImGuiMCP::ImGuiKey_Y, 21},
-                                      {ImGuiMCP::ImGuiKey_U, 22},
-                                      {ImGuiMCP::ImGuiKey_I, 23},
-                                      {ImGuiMCP::ImGuiKey_O, 24},
-                                      {ImGuiMCP::ImGuiKey_P, 25},
-                                      {ImGuiMCP::ImGuiKey_LeftBracket, 26},
-                                      {ImGuiMCP::ImGuiKey_RightBracket, 27},
-                                      {ImGuiMCP::ImGuiKey_Enter, 28},
-                                      {ImGuiMCP::ImGuiKey_LeftCtrl, 29},
-                                      {ImGuiMCP::ImGuiKey_A, 30},
-                                      {ImGuiMCP::ImGuiKey_S, 31},
-                                      {ImGuiMCP::ImGuiKey_D, 32},
-                                      {ImGuiMCP::ImGuiKey_F, 33},
-                                      {ImGuiMCP::ImGuiKey_G, 34},
-                                      {ImGuiMCP::ImGuiKey_H, 35},
-                                      {ImGuiMCP::ImGuiKey_J, 36},
-                                      {ImGuiMCP::ImGuiKey_K, 37},
-                                      {ImGuiMCP::ImGuiKey_L, 38},
-                                      {ImGuiMCP::ImGuiKey_Semicolon, 39},
-                                      {ImGuiMCP::ImGuiKey_Apostrophe, 40},
-                                      {ImGuiMCP::ImGuiKey_GraveAccent, 41},
-                                      {ImGuiMCP::ImGuiKey_LeftShift, 42},
-                                      {ImGuiMCP::ImGuiKey_Backslash, 43},
-                                      {ImGuiMCP::ImGuiKey_Z, 44},
-                                      {ImGuiMCP::ImGuiKey_X, 45},
-                                      {ImGuiMCP::ImGuiKey_C, 46},
-                                      {ImGuiMCP::ImGuiKey_V, 47},
-                                      {ImGuiMCP::ImGuiKey_B, 48},
-                                      {ImGuiMCP::ImGuiKey_N, 49},
-                                      {ImGuiMCP::ImGuiKey_M, 50},
-                                      {ImGuiMCP::ImGuiKey_Comma, 51},
-                                      {ImGuiMCP::ImGuiKey_Period, 52},
-                                      {ImGuiMCP::ImGuiKey_Slash, 53},
-                                      {ImGuiMCP::ImGuiKey_RightShift, 54},
-                                      {ImGuiMCP::ImGuiKey_KeypadMultiply, 55},
-                                      {ImGuiMCP::ImGuiKey_LeftAlt, 56},
-                                      {ImGuiMCP::ImGuiKey_Space, 57},
-                                      {ImGuiMCP::ImGuiKey_CapsLock, 58},
-                                      {ImGuiMCP::ImGuiKey_F1, 59},
-                                      {ImGuiMCP::ImGuiKey_F2, 60},
-                                      {ImGuiMCP::ImGuiKey_F3, 61},
-                                      {ImGuiMCP::ImGuiKey_F4, 62},
-                                      {ImGuiMCP::ImGuiKey_F5, 63},
-                                      {ImGuiMCP::ImGuiKey_F6, 64},
-                                      {ImGuiMCP::ImGuiKey_F7, 65},
-                                      {ImGuiMCP::ImGuiKey_F8, 66},
-                                      {ImGuiMCP::ImGuiKey_F9, 67},
-                                      {ImGuiMCP::ImGuiKey_F10, 68},
-                                      {ImGuiMCP::ImGuiKey_NumLock, 69},
-                                      {ImGuiMCP::ImGuiKey_ScrollLock, 70},
-                                      {ImGuiMCP::ImGuiKey_Keypad7, 71},
-                                      {ImGuiMCP::ImGuiKey_Keypad8, 72},
-                                      {ImGuiMCP::ImGuiKey_Keypad9, 73},
-                                      {ImGuiMCP::ImGuiKey_KeypadSubtract, 74},
-                                      {ImGuiMCP::ImGuiKey_Keypad4, 75},
-                                      {ImGuiMCP::ImGuiKey_Keypad5, 76},
-                                      {ImGuiMCP::ImGuiKey_Keypad6, 77},
-                                      {ImGuiMCP::ImGuiKey_KeypadAdd, 78},
-                                      {ImGuiMCP::ImGuiKey_Keypad1, 79},
-                                      {ImGuiMCP::ImGuiKey_Keypad2, 80},
-                                      {ImGuiMCP::ImGuiKey_Keypad3, 81},
-                                      {ImGuiMCP::ImGuiKey_Keypad0, 82},
-                                      {ImGuiMCP::ImGuiKey_KeypadDecimal, 83},
-                                      {ImGuiMCP::ImGuiKey_F11, 87},
-                                      {ImGuiMCP::ImGuiKey_F12, 88},
-                                      {ImGuiMCP::ImGuiKey_KeypadEnter, 156},
-                                      {ImGuiMCP::ImGuiKey_RightCtrl, 157},
-                                      {ImGuiMCP::ImGuiKey_KeypadDivide, 181},
-                                      {ImGuiMCP::ImGuiKey_PrintScreen, 183},
-                                      {ImGuiMCP::ImGuiKey_RightAlt, 184},
-                                      {ImGuiMCP::ImGuiKey_Pause, 197},
-                                      {ImGuiMCP::ImGuiKey_Home, 199},
-                                      {ImGuiMCP::ImGuiKey_UpArrow, 200},
-                                      {ImGuiMCP::ImGuiKey_PageUp, 201},
-                                      {ImGuiMCP::ImGuiKey_LeftArrow, 203},
-                                      {ImGuiMCP::ImGuiKey_RightArrow, 205},
-                                      {ImGuiMCP::ImGuiKey_End, 207},
-                                      {ImGuiMCP::ImGuiKey_DownArrow, 208},
-                                      {ImGuiMCP::ImGuiKey_PageDown, 209},
-                                      {ImGuiMCP::ImGuiKey_Insert, 210},
-                                      {ImGuiMCP::ImGuiKey_Delete, 211},
-                                      {ImGuiMCP::ImGuiKey_GamepadDpadUp, 266},
-                                      {ImGuiMCP::ImGuiKey_GamepadDpadDown, 267},
-                                      {ImGuiMCP::ImGuiKey_GamepadDpadLeft, 268},
-                                      {ImGuiMCP::ImGuiKey_GamepadDpadRight, 269},
-                                      {ImGuiMCP::ImGuiKey_GamepadStart, 270},
-                                      {ImGuiMCP::ImGuiKey_GamepadBack, 271},
-                                      {ImGuiMCP::ImGuiKey_GamepadL3, 272},
-                                      {ImGuiMCP::ImGuiKey_GamepadR3, 273},
-                                      {ImGuiMCP::ImGuiKey_GamepadL1, 274},
-                                      {ImGuiMCP::ImGuiKey_GamepadR1, 275},
-                                      {ImGuiMCP::ImGuiKey_GamepadFaceDown, 276},
-                                      {ImGuiMCP::ImGuiKey_GamepadFaceRight, 277},
-                                      {ImGuiMCP::ImGuiKey_GamepadFaceLeft, 278},
-                                      {ImGuiMCP::ImGuiKey_GamepadFaceUp, 279},
-                                      {ImGuiMCP::ImGuiKey_GamepadL2, 280},
-                                      {ImGuiMCP::ImGuiKey_GamepadR2, 281}};
-
-        for (const auto& m : keys) {
-            if (ImGuiMCP::IsKeyPressed(static_cast<ImGuiMCP::ImGuiKey>(m.key), false)) {
-                return m.dik;
-            }
-        }
-
-        if (ImGuiMCP::IsMouseClicked(ImGuiMCP::ImGuiMouseButton_Left, false)) {
-            return 256;
-        }
-
-        if (ImGuiMCP::IsMouseClicked(ImGuiMCP::ImGuiMouseButton_Right, false)) {
-            return 257;
-        }
-
-        if (ImGuiMCP::IsMouseClicked(ImGuiMCP::ImGuiMouseButton_Middle, false)) {
-            return 258;
-        }
-
-        if (ImGuiMCP::IsMouseClicked(3, false)) {
-            return 259;
-        }
-
-        if (ImGuiMCP::IsMouseClicked(4, false)) {
-            return 260;
-        }
-
-        return -1;
-    }
 }
 
 namespace SettingsUI {
@@ -187,6 +25,7 @@ namespace SettingsUI {
         }
 
         SKSEMenuFramework::SetSection("Eternal Buffs");
+        SKSEMenuFramework::AddSectionItem("Settings", SettingsUI::Render);
         SKSEMenuFramework::AddSectionItem("Settings", SettingsUI::Render);
         logger::info("[Eternal buffs] Registered SKSE Menu Framework settings.");
     }
@@ -410,8 +249,6 @@ namespace SettingsUI {
             ImGuiMCP::EndTable();
         }
 
-        // ImGuiMCP::SetWindowFontScale(1.0f);
-
         for (RE::FormID id : toRemove) {
             SpellDataPersistence::RemoveSpellFromSave(id);
         }
@@ -431,7 +268,7 @@ namespace SettingsUI {
             {"Scrolls", "Allows scrolls to be affected. Recommended to be disabled."},
             {"Recastable",
              "If enabled, non-recastable spells are affected. Some spells can not be recasted while their effect is "
-             "still running. They must be dispelled manually."},
+             "still running. They must be dispelled manually. Some cloak spells have non-recastable effects."},
             // {"ReserveMagicka", "Not implemented. Reserves magicka for this spell type."},
         };
 
@@ -441,14 +278,8 @@ namespace SettingsUI {
         for (const auto& [name, boolPtr] : generalRule.GetFieldsMatchedConfigFile()) {
             if (!boolPtr) continue;
 
-            // ImGuiMCP::Checkbox(name.c_str(), boolPtr);
-
             if (ImGuiMCP::Checkbox(name.c_str(), boolPtr)) {
-                // *boolPtr already holds the new value here.
                 configLoader.UpdateConfigValue(config.ConfigFilePath, "General", name, *boolPtr ? "true" : "false");
-
-                // .GetConfigLoader().UpdateConfigValue(config.ConfigFilePath, "general", name,
-                //                                            *boolPtr ? "true" : "false");
             }
 
             if (auto it = descriptions.find(name); it != descriptions.end()) {
@@ -458,17 +289,6 @@ namespace SettingsUI {
     }
 
     void __stdcall Render() {
-        // static bool wasOpenLastFrame = false;
-        // bool isOpenThisFrame = true; // Render is only called while this tab is visible
-
-        // if (isOpenThisFrame && !wasOpenLastFrame) {
-        //     if (auto* player = RE::PlayerCharacter::GetSingleton()) {
-        //         logger::debug("Pruning unapplied saved spells.");
-        //         PruneUnappliedSavedSpells(*player);
-        //     }
-        // }
-        // wasOpenLastFrame = isOpenThisFrame;
-
         RenderGeneralRuleGeneric();
         RenderSavedSpellsTable();
         RenderSpellRulesTable();
